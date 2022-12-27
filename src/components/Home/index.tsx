d92fb6388111887b8eb7c005472a0c144095a0f8
Sarthak Verma <55709030+sarthakvdev@users.noreply.github.com>
2022-12-27 23:38:38 +0530

    init setup of next for proj

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Lens API 101</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        Hello LENS (🌿, 🌸)
+      </main>
+    </div>
+  );
+};
+
+export default Home;
